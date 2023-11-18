257c7feef7edcaee45979066a67fa5d96f1a3146
Carlos Cortez <thehome@live.com.ar>
2023-11-18 20:13:21 -0300

    nueva carga card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+
+import { useEffect } from 'react';
+import axios from "axios";
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarComponent from './components/NavBarcomponent/NavBarComponent';
 import './index.css'
 import ItemListConteinerComponent from './components/itemListConteinerComponent/itemListConteinerComponent';
 
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://dummyjson.com/products")
+      .then((res) => {
+        setProducts(res.data.products);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+
+
   return (
     <div>
       <NavBarComponent />
-      <ItemListConteinerComponent greeting="Bienvenidos a CUBO indumentaria" />
+      <ItemListConteinerComponent products={products} />
     </div>
   );
 }
--- src/components/ItemListConteinerComponent/ItemListConteinerComponent.jsx
+++ src/components/ItemListConteinerComponent/ItemListConteinerComponent.jsx
@@ -1,24 +1,33 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
 
 
-const ItemListConteinerComponent = ({greeting}) => {
-
-const greetingStyle = {
-    fontSize: '3rem',
-    fontWeight: '700',
-    textAlign: 'center',
-    color:'white',
-    paddingTop:'20%',
-
-};
-
-
-
-return(
-    <div style={greetingStyle}>
-{greeting}
+const ItemListConteinerComponent = ({products}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        width: "100vw",
+        justifyContent: "space-around",
+      }}
+    >
+       {products.map((product) => {
+        return (
+          <Card key={product.id} style={{ width: "18rem", margin: 20 }}>
+            
+              <Card.Img variant="top" src={product.thumbnail} />
+            
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>{product.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
-)
-
+  );
+    
 }
 
-export default ItemListConteinerComponent;
+export default ItemListConteinerComponent
